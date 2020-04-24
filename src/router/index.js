b6d050c64e5ff5f3cdefbc21acd542e9e76cb528
fuxiangji <4968459@qq.com>
2020-04-24 11:46:07 +0800

    路由守卫

--- src/router/index.js
+++ src/router/index.js
@@ -5,18 +5,17 @@ import Home from '../components/Home.vue'
 
 Vue.use(VueRouter)
 
-  const routes = [
-  {
+const routes = [{
     path: '/',
     redirect: '/login',
   },
   {
     path: '/login',
-    component:Login
+    component: Login
   },
   {
     path: '/home',
-    component:Home
+    component: Home
   }
 ]
 
@@ -26,5 +25,15 @@ const router = new VueRouter({
   routes
 })
 // 挂载路由守卫
+router.beforeEach((to, from, next) => {
+  // to将要访问的路径
+  // from代表从哪个路径跳转而来
+  // next 是一个函数 ，表示放行 next（路径）
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
 
 export default router
