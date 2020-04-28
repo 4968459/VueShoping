417c8712def524b650c7eda79e811c6c50df3cac
fuxiangji <4968459@qq.com>
2020-04-29 01:04:06 +0800

    完成了商品分类模块！

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -36,9 +36,9 @@
         <el-tag size="mini" type="warning" v-else>三级</el-tag>
       </template>
       <!-- 操作 -->
-      <template slot="opst">
-        <el-button type="primary" size="mini" icon="el-icon-edit">编辑</el-button>
-        <el-button type="danger" size="mini" icon="el-icon-delete">删除</el-button>
+      <template slot="opst" slot-scope="scope">
+        <el-button type="primary" size="mini" @click="editCate(scope)" icon="el-icon-edit">编辑</el-button>
+        <el-button type="danger" size="mini" @click="deleteCate(scope)" icon="el-icon-delete">删除</el-button>
       </template>
     </tree-table>
 
@@ -53,7 +53,12 @@
       :total="total"
     ></el-pagination>
     <!-- 添加分类对话框 -->
-    <el-dialog title="添加分类" :visible.sync="AddClassdialogVisible" width="50%">
+    <el-dialog
+      @close="addCateDialogClosed"
+      title="添加分类"
+      :visible.sync="AddClassdialogVisible"
+      width="50%"
+    >
       <el-form
         :model="addclassruleForm"
         :rules="addclassruleFormrules"
@@ -63,10 +68,40 @@
         <el-form-item label="分类名称：" prop="cat_name">
           <el-input v-model="addclassruleForm.cat_name"></el-input>
         </el-form-item>
+        <el-form-item label="父级分类：">
+          <el-cascader
+            v-model="selectKeys"
+            :options="paerntCateList"
+            :props="{ expandTrigger: 'hover', children:'children',value:'cat_id',label:'cat_name' }"
+            size="medium"
+            @change="changeCascader()"
+            clearable
+            change-on-select
+            style="width:100%"
+          ></el-cascader>
+        </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="AddClassdialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="AddClassdialogVisible = false">确 定</el-button>
+        <el-button type="primary" @click="addCate">确 定</el-button>
+      </span>
+    </el-dialog>
+    <!-- 修改分类对话框 -->
+    <el-dialog
+      title="修改分类名称"
+      @close="editdialogClosed"
+      :visible.sync="EditCenterDialogVisible"
+      width="30%"
+      center
+    >
+      <el-form :model="editRuleForm" :rules="editRules" ref="editRuleFormRef" label-width="100px">
+        <el-form-item label="分类名称" prop="cat_name">
+          <el-input v-model="editRuleForm.cat_name"></el-input>
+        </el-form-item>
+      </el-form>
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="EditCenterDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="editCateName">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -118,7 +153,23 @@ export default {
         cat_name: [
           { required: true, message: "请输入分类名称", trigger: "blur" }
         ]
-      }
+      },
+      // 父级分类列表
+      paerntCateList: [],
+      // 级联选择器选中的项目数组
+      selectKeys: [],
+      // 修改分类名称对话框
+      EditCenterDialogVisible: false,
+      editRuleForm: {
+        cat_name: ""
+      },
+      editRules: {
+        cat_name: [
+          { required: true, message: "请输分类名称", trigger: "blur" },
+          { min: 1, max: 10, message: "长度在 1 到 10 个字符", trigger: "blur" }
+        ]
+      },
+      editId: 0
     };
   },
   created() {
@@ -148,7 +199,110 @@ export default {
       this.getCatelist();
     },
     showaddclassdialog() {
+      // 获取父级分类列表数据
+      this.getparentCateList();
       this.AddClassdialogVisible = true;
+    },
+    async getparentCateList() {
+      const { data: res } = await this.$http.get(`categories`, {
+        params: { type: 2 }
+      });
+      if (res.meta.status !== 200) {
+        this.$message.error("获取父级分类列表失败！");
+      }
+      console.log(res.data);
+      this.paerntCateList = res.data;
+    },
+    // 修改了选择触发函数
+    changeCascader() {
+      if (this.selectKeys.length > 0) {
+        this.addclassruleForm.cat_pid = this.selectKeys[
+          this.selectKeys.length - 1
+        ];
+        this.addclassruleForm.cat_level = this.selectKeys.length;
+      } else {
+        this.addclassruleForm.cat_pid = 0;
+        this.addclassruleForm.cat_level = 0;
+      }
+    },
+    addCate() {
+      this.$refs.addclassruleFormRef.validate(async valid => {
+        if (!valid) {
+          return;
+        }
+        const { data: res } = await this.$http.post(
+          `categories`,
+          this.addclassruleForm
+        );
+        if (res.meta.status !== 201) {
+          return this.$message.error("添加分类失败！");
+        }
+        this.$message.success("添加分类成功");
+        this.getCatelist();
+        this.AddClassdialogVisible = false;
+      });
+    },
+    // 添加分类对话框关闭事件
+    addCateDialogClosed() {
+      this.$refs.addclassruleFormRef.resetFields();
+      this.addclassruleForm.cat_level = 0;
+      this.addclassruleForm.cat_pid = 0;
+      this.selectKeys = [];
+    },
+    // 删除方法
+    deleteCate(scopeData) {
+      this.$confirm("此操作将永久删除该分类, 是否继续?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+        center: true
+      })
+        .then(async () => {
+          const { data: res } = await this.$http.delete(
+            `categories/${scopeData.row.cat_id}`
+          );
+          if (res.meta.status !== 200) {
+            return $message.error("删除失败！");
+          }
+          this.$message({
+            type: "success",
+            message: "删除成功!"
+          });
+          this.getCatelist();
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
+    },
+    // 修改分类名称
+   async editCate(spocedData) {
+      this.EditCenterDialogVisible = true;
+      this.editId = spocedData.row.cat_id;
+      const { data: res } = await this.$http.get(`categories/${this.editId}`)
+      if (res.meta.status !== 200) {
+       return this.$message.error("获取名称失败！")
+      }
+      this.editRuleForm.cat_name = res.data.cat_name
+    },
+    async editCateName() {
+      const { data: res } = await this.$http.put(
+        `categories/${this.editId}`,
+        this.editRuleForm
+      );
+      console.log(res);
+
+      if (res.meta.status !== 200) {
+        return this.$message.error("修改分类名称失败！");
+      }
+      this.$message.success("修改成功！");
+      this.EditCenterDialogVisible = false;
+      this.getCatelist();
+    },
+    editdialogClosed() {
+      this.editRuleForm.cat_name = "";
     }
   }
 };
