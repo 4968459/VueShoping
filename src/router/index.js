bcb58703903779a302a2598ae5eab870f4760b62
fuxiangji <4968459@qq.com>
2020-05-02 16:37:29 +0800

    完成了数据报表

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,7 @@ import Params from '../components/goods/Params.vue'
 import GoosList from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue';
 import Order from '../components/order/Order.vue'
+import Report from '@/components/report/Report'
 
 Vue.component('tree-table', TreeTable)
 Vue.use(VueRouter)
@@ -55,6 +56,9 @@ const routes = [{
     }, {
       path: "/orders",
       component: Order
+    }, {
+      path: '/reports',
+      component: Report
     }]
   }
 ]
